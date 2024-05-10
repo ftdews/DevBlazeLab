533ae9b100d8cd132f377a806cecdf119edafe6d
ftdews <gs1kweku707@hotmail.com>
2024-05-10 23:15:25 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-function wordBreak(s, wordDict) {
-  const wordSet = new Set(wordDict);
-  const dp = new Array(s.length + 1).fill(false);
-  dp[0] = true;
-  for (let end = 1; end <= s.length; end++) {
-    for (let start = 0; start < end; start++) {
-      if (dp[start] && wordSet.has(s.substring(start, end))) {
-        dp[end] = true;
-        break;
-      }
-    }
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
   }
-  return dp[s.length];
+  return maxProfit;
 }
