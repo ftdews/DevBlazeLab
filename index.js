861a9470141935e26f6a8e4fbe6f8b1c02f24a8b
ftdews <gs1kweku707@hotmail.com>
2024-05-13 04:47:05 +0700

    start

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-function largestNumber(nums) {
-  if (nums.every((num) => num === 0)) return "0";
-  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
-}
+const uniqueValues = (arr) => [...new Set(arr)];
